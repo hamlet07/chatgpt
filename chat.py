3f58b4aa6ea253078afada7e16b7f711b353c7f0
hamlet07 <hamlet07@o2.pl>
2023-01-28 14:13:49 +0100

    Changes

--- chat.py
+++ chat.py
@@ -3,6 +3,7 @@ import customtkinter
 import openai
 import os
 import pickle
+from PIL import Image
 
 root = customtkinter.CTk()
 root.title("ChatGPT Bot")
@@ -27,7 +28,7 @@ def speak():
                     model = "text-davinci-003",
                     prompt = chat_entry.get(),
                     temperature = 0,
-                    max_tokens = 60,
+                    max_tokens = 250,
                     top_p = 1.0,
                     frequency_penalty = 0.0,
                     presence_penalty = 0.0, 
@@ -53,7 +54,6 @@ def clear():
     chat_entry.delete(0, END)
 
 def key():
-    root.geometry("600x580")
     api_frame.pack(pady=30)
     filename = "api_key"
 
@@ -77,14 +77,12 @@ def save_key():
 
         api_entry.delete(0, END)
 
-        api_frame.pack_forget()
-        root.geometry("600x420")
     except Exception as e:
         my_text.insert(END, f"\n\n There was an error\n\n{e}")
 
 #Text
 text_frame = customtkinter.CTkFrame(root)
-text_frame.pack(pady=20)
+text_frame.pack(pady=5)
 
 my_text = Text(text_frame, 
     bg = "#343638",     
@@ -93,7 +91,8 @@ my_text = Text(text_frame,
     fg="#d6d6d6",
     relief = "flat",
     wrap=WORD,
-    selectbackground="#1f538d")
+    #selectbackground="#1f538d"
+    )
 my_text.grid(row=0, column=0)
 
 text_scroll = customtkinter.CTkScrollbar(text_frame, command=my_text.yview)
@@ -101,44 +100,44 @@ text_scroll.grid(row=0, column=1, sticky="ns")
 
 my_text.configure(yscrollcommand=text_scroll.set)
 
-chat_entry = customtkinter.CTkEntry(root, 
+img1=customtkinter.CTkImage(Image.open("icons8-broom-24.png"))
+
+clear_button = customtkinter.CTkButton(text_frame,
+    image = img1,
+    text = "",
+    command = clear)
+clear_button.grid(row=0, column=2, padx=5)
+
+chat_entry_frame = customtkinter.CTkFrame(root)
+chat_entry_frame.pack(pady=5)
+
+chat_entry = customtkinter.CTkEntry(chat_entry_frame, 
     placeholder_text="Type Something to ChatGPT...",
-    width=535,
+    width=350,
     height=50,
     border_width=1)
-chat_entry.pack(pady=10)
+chat_entry.grid(row=0, column=0, padx=5, pady=5)
 
-#Buttons
-button_frame = customtkinter.CTkFrame(root, fg_color="#242424")
-button_frame.pack(pady=10)
+img2=customtkinter.CTkImage(Image.open("icons8-email-send-30.png"))
 
-submit_button = customtkinter.CTkButton(button_frame,
-    text = "Speak to ChatGPT",
+submit_button = customtkinter.CTkButton(chat_entry_frame,
+    image = img2,
+    text = "",
     command = speak)
-submit_button.grid(row=0, column=0, padx=15)
-
-clear_button = customtkinter.CTkButton(button_frame,
-    text = "Clear Response",
-    command = clear)
-clear_button.grid(row=0, column=1, padx=15)
-
-api_button = customtkinter.CTkButton(button_frame,
-    text = "Update API Key",
-    command = key)
-api_button.grid(row=0, column=2, padx=15)
+submit_button.grid(row=0, column=1, padx=5)
 
 #API
-api_frame = customtkinter.CTkFrame(root, border_width = 1)
-api_frame.pack(pady=10)
+api_frame = customtkinter.CTkFrame(root)
+api_frame.pack(pady=5)
 
 api_entry = customtkinter.CTkEntry(api_frame, 
     placeholder_text="Enter your API Key",
     width = 350, height = 50, border_width=1)
-api_entry.grid(row=0, column=0, padx=20, pady=20)
+api_entry.grid(row=0, column=0, padx=5, pady=5)
 
 api_save_button = customtkinter.CTkButton(api_frame,
     text="Save key",
     command = save_key)
-api_save_button.grid(row=0, column=1, padx=10)    
+api_save_button.grid(row=0, column=1, padx=5)    
 
 root.mainloop()
